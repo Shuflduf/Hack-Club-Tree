96dba9d8cb0e61a8c53243d3bb7981b263d7bfc0
Shuflduf <shuflduf@gmail.com>
2025-12-16 22:01:11 -0700

    im so fucking good at css

--- src/routes/OrnamentOptions.svelte
+++ src/routes/OrnamentOptions.svelte
@@ -2,13 +2,21 @@
 	<form>
 		<div class="question">
 			<h1>Transform</h1>
-			<div class="spacer" />
+			<div class="spacer"></div>
 			<div class="answer">
-				<img src="https://icons.hackclub.com/api/icons/0xf8e9d3/view-reload" alt="rotation" />
+				<img
+					draggable="false"
+					src="https://icons.hackclub.com/api/icons/0xf8e9d3/view-reload"
+					alt="rotation"
+				/>
 				<input type="range" min="-180" max="180" step="10" />
 			</div>
 			<div class="answer">
-				<h2>Flipped</h2>
+				<img
+					draggable="false"
+					src="https://icons.hackclub.com/api/icons/0xf8e9d3/expand"
+					alt="flip"
+				/>
 				<input type="checkbox" />
 			</div>
 			<div class="subtitle">Position related info</div>
@@ -28,7 +36,7 @@
 		z-index: 20;
 	}
 	form {
-		font-family: "Jua";
+		font-family: 'Jua';
 		width: min(600px, 80%);
 
 		padding: 32px;
@@ -47,16 +55,18 @@
 	.question {
 		display: flex;
 		flex-direction: column;
+		gap: 16px;
 		background-color: #dd9f40;
 		margin-top: 40px;
 		position: relative;
 		border-radius: 0 10px 10px 10px;
 		color: #f8e9d3;
+		box-shadow: var(--shadow);
 
 		h1 {
 			font-size: 1.25rem;
 			position: absolute;
-			top: -30px;
+			top: -25px;
 			left: 0;
 			margin: 0;
 			padding: 8px 24px;
@@ -66,7 +76,7 @@
 		}
 
 		.spacer {
-			height: 12px;
+			height: 4px;
 		}
 
 		.answer {
@@ -81,26 +91,64 @@
 			appearance: none;
 			border-radius: 0 8px 8px 0;
 			cursor: pointer;
+			box-shadow: var(--shadow);
 		}
-		input[type=range]::-moz-range-track{
+
+		input[type='checkbox'] {
+			background-color: #f8e9d3;
+			width: 48px;
+			cursor: pointer;
+			position: relative;
+		}
+		input[type='checkbox']::before {
+			content: '';
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+
+			width: 24px;
+			height: 24px;
+			border: 2px solid #bf782b;
+			border-radius: 8px;
+		}
+		input[type='checkbox']:checked::before {
+			content: '';
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+
+			width: 24px;
+			height: 24px;
+			background-color: #bf782b;
+			border-radius: 8px;
+		}
+
+		input[type='range']::-moz-range-track {
 			background-color: #bf782b;
 			height: 2px;
 		}
-		input[type=range]::-moz-range-thumb {
-		  background: #f8e9d3;
-		  border-radius: 50%;
-		  // border-color: #bf782b;
-		  border: 2px solid #bf782b;
-}
+		input[type='range']::-moz-range-thumb {
+			background: #f8e9d3;
+			border-radius: 50%;
+			border: 2px solid #bf782b;
+		}
 
 		img {
 			background-color: #bf782b;
 			border-radius: 8px 0 0 8px;
 			height: 48px;
+			user-select: none;
+			box-shadow: var(--shadow);
 		}
 
 		.subtitle {
 			background-color: #e7bf6e;
+			border-radius: 0 0 8px 8px;
+			padding: 8px;
+			color: #884b2b;
+			font-size: 0.875rem;
 		}
 	}
 </style>

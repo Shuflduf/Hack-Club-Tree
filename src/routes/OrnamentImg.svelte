a769088cf9baeff45f5929adcd300f4edacba5d1
Shuflduf <shuflduf@gmail.com>
2025-12-17 19:44:21 -0700

    clicking to open info

--- src/routes/OrnamentImg.svelte
+++ src/routes/OrnamentImg.svelte
@@ -1,25 +1,11 @@
 <script lang="ts">
 	import './ornament_img.css';
 	import { Decoration, DecorationFiles, type Ornament, type OrnamentConfig } from '$lib';
+	import { onMount } from 'svelte';
 
-	// let {
-
-	// 	position,
-	// 	placed,
-	// 	src,
-	// 	config,
-	// 	display_name,
-	// 	slack_id
-	// }: {
-	// 	position: [number, number];
-	// 	placed: boolean;
-	// 	src: string;
-	// 	config: OrnamentConfig;
-	// 	display_name: string;
-	// 	slack_id: string;
-	// } = $props();
 	let { orn, placed }: { orn: Ornament; placed: boolean } = $props();
 	let position = $derived(orn.ornament_position);
+	let infoOpened = $state(false);
 
 	let decorationUrl = $derived(DecorationFiles[orn.decoration_index as Decoration]);
 
@@ -28,31 +14,42 @@
 		return `${date.toLocaleDateString('en-GB', { day: 'numeric', month: 'short', year: 'numeric' })}`;
 		// return `${date.getDay()} ${date.toLocaleDateString}, ${date.getFullYear()}`
 	}
+
+	onMount(() => {
+		// this is stupid but its fine
+		document.addEventListener('mousedown', (ev: MouseEvent) => {
+			infoOpened = false;
+		});
+	});
 </script>
 
 <div class="ornament" style={`top: ${position[1]}px; left: ${position[0]}px`}>
-	<img
-		src={orn.pfp_url}
-		draggable="false"
-		class={`profile ${placed ? '' : 'pulsing'}`}
-		alt="profile"
-		style={`rotate: ${orn.rotation}deg; transform: scaleX(${orn.flipped ? -1 : 1})`}
-	/>
+	<button class="no-button" onclick={() => (infoOpened = true)}>
+		<img
+			src={orn.pfp_url}
+			draggable="false"
+			class={`profile ${placed ? '' : 'pulsing'}`}
+			alt="profile"
+			style={`rotate: ${orn.rotation}deg; transform: scaleX(${orn.flipped ? -1 : 1})`}
+		/>
+	</button>
 	{#if orn.decoration_index != 0}
 		<img src={decorationUrl} class="overlay" alt="overlay" draggable="false" />
 	{/if}
-	<div class="info">
-		<a href={`https://hackclub.slack.com/team/${orn.slack_id}`}>{orn.username}</a>
+	{#if infoOpened}
+		<div class="info">
+			<a href={`https://hackclub.slack.com/team/${orn.slack_id}`}>{orn.username}</a>
 
-		<div class="likes">
-			<button>
-				<img src="https://icons.hackclub.com/api/icons/0xf8e9d3/thumbsup-fill" alt="like" />
-			</button>
-			<p>42</p>
+			<div class="likes">
+				<button>
+					<img src="https://icons.hackclub.com/api/icons/0xf8e9d3/thumbsup-fill" alt="like" />
+				</button>
+				<p>42</p>
+			</div>
+			<div class="created-at">
+				<span>Created at: {formatDate(orn.created_at)}</span>
+				<span>Updated at: {formatDate(orn.updated_at)}</span>
+			</div>
 		</div>
-		<div class="created-at">
-			<span>Created at: {formatDate(orn.created_at)}</span>
-			<span>Updated at: {formatDate(orn.updated_at)}</span>
-		</div>
-	</div>
+	{/if}
 </div>

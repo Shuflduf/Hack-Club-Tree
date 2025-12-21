0f6f558cca66051f345274e33fb29cc6bd2eb19d
Shuflduf <shuflduf@gmail.com>
2025-12-20 18:48:56 -0700

    title slop

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
 	import './global.css';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.png" />
+	<title>HC Tree</title>
+	<meta name="description" content="Place your profile picture on the tree to celebrate the December Holidays!"/>
 </svelte:head>
 
 {@render children()}
